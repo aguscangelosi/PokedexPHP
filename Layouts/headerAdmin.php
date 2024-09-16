eff6a7d7dff49ea8cb2a58a7d32b13b903c3aea6
MartinMeza22 <martinmeza.t22@gmail.com>
2024-09-15 23:36:16 -0300

    Header principal, header admin, style

---
+++ Layouts/headerAdmin.php
@@ -0,0 +1,32 @@
+headerPrincipal.php<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="../Styles/layoutHeaderAdmin.css">
+    <title>Header</title>
+</head>
+<body>
+<header>
+    <!--Falta agregar form-->
+    <nav class="nav">
+        <div class = "contenedor-nav">
+            <div class="contenedor-isologo">
+                <img class= "logo" src="../Imagenes/isologo.png" alt="Isologotipo">
+            </div>
+            <div class="contenedor-logo">
+                <img class="logo" src="../Imagenes/LogoPokedex.png" alt="Logo Pokedex">
+            </div>
+            <div class="contenedor-sesion">
+                <h3>Usuario: Admin</h3>
+            </div>
+        </div>
+        <div class="input-principal">
+            <input id="input-nav" type="text" placeholder="   Ingrese el nombre, tipo o número de pókemon">
+            <a>Quien es este pokemón?</a>
+        </div>
+
+    </nav>
+</header>
+</body>
+</html>
---
+++ Layouts/headerPrincipal.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="../Styles/layoutsHeaderPrincipal.css">
+    <title>Header</title>
+</head>
+<body>
+<header>
+    <!--Falta agregar form-->
+    <nav class="nav">
+        <div class = "contenedor-nav">
+            <div class="contenedor-isologo">
+                <img class= "logo" src="../Imagenes/isologo.png" alt="Isologotipo">
+            </div>
+            <div class="contenedor-logo">
+                <img class="logo" src="../Imagenes/LogoPokedex.png" alt="Logo Pokedex">
+            </div>
+            <div class="contenedor-sesion">
+                <input placeholder="Hash Kechu" disabled> <!--Cambiar etiqueta-->
+                <input placeholder="*********" disabled> <!--Cambiar etiqueta-->
+                <button id="boton-ingreso">Ingresar</button>
+            </div>
+        </div>
+        <div class="input-principal">
+            <input id="input-nav" type="text" placeholder="   Ingrese el nombre, tipo o número de pókemon">
+            <a>Quien es este pokemón?</a>
+        </div>
+
+    </nav>
+</header>
+</body>
+</html>
