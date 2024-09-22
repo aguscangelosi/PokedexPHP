a25bcacdd864c315d29ad6cd313d0798563dab76
Iván Coronel <imc8046@gmail.com>
2024-09-22 19:24:27 -0300

    Modifique los formularios de registro, agregar pokemon, y modifique un poco el footer

--- Layouts/footer.php
+++ Layouts/footer.php
@@ -1,5 +1,13 @@
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+</main>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
             integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
             crossorigin="anonymous"></script>
 </body>
+<footer class="p-3 bg-dark" >
+    <ul class="nav justify-content-center">
+        <li class="nav-item">
+            <a class="nav-link fw-light text-white" aria-current="page" href="#">Programación Web II - Grupo 15</a>
+        </li>
+    </ul>
+</footer>
 </html>
--- Paginas/agregarPokemon.php
+++ Paginas/agregarPokemon.php
@@ -1,33 +1,64 @@
 <?php
-include ('../Layouts/headerAdmin.php');
+include ("../Layouts/header.php");
 ?>
-<head>
-    <link rel="stylesheet" href="../Styles/agregarPokemon.css">
-</head>
-<main>
-    <h1>Añadir Pokemón</h1>
-    <div id="contenedor-principal">
-        <form id="agregar-pokemon" action="logicaAgregarPokemon.php" method="post">
-            <label>Ingrese nombre del pokemon</label>
-                <input class="input-add" type="text" name="nombre-pokemon" required>
-            <label>Ingrese numero de identificación</label>
-                <input class="input-add" type="number" name="numero-identficador" required>
-            <label>Ingrese tipo de pokemon</label>
-                <select class="input-add" multiple name="tipo-pokemon" required>
-                    <option value="agua">Agua</option>
-                    <option value="dragon">Dragón</option>
-                    <option value="electrico">Electrico</option>
-                    <option value="fantasma">Fastasma</option>
-                    <option value="fuego">Fuego</option>
-                    <option value="normal">Normal</option>
-                    <option value="hierba">Bob Marley</option>
-                    <option value="psiquico">Psiquico</option>
-                </select>
-            <label>Descripción del pokemon:</label>
-                <input class="input-add" type="text" name="descripcion-pokemon" required>
-            <label>Información: </label>
-                <input class="input-add" type="text" name="informacion-pokemon" required>
-            <input class="button-add" type="submit">
-        </form>
-    </div>
-</main>
+
+    <h1 class="fw-bold text-center p-3">Añadir Pokemón</h1>
+
+    <form id="agregar-pokemon mt-3" action="logicaAgregarPokemon.php" method="post">
+
+        <div class="container text-center">
+            <div class="row justify-content-center">
+                <div class="col-md-4 mx-auto p-2">
+
+                    <div class="form-floating m-2">
+                        <input type="text" class="form-control" placeholder="" name="nombre-pokemon" required>
+                        <label for="floatingInput">Nombre del Pokémon</label>
+                    </div>
+
+                    <div class="form-floating m-2">
+                        <input type="number" class="form-control" name="numero-identficador" placeholder="" required>
+                        <label for="floatingInput">Número de identificación</label>
+                    </div>
+
+                    <div class="m-2">
+                        <select class="form-select" multiple aria-label="Multiple select example" multiple name="tipo-pokemon" required>
+                            <option value="normal">Normal</option>
+                            <option value="agua">Planta</option>
+                            <option value="agua">Agua</option>
+                            <option value="fuego">Fuego</option>
+                            <option value="dragon">Dragón</option>
+                            <option value="electrico">Eléctrico</option>
+                            <option value="fantasma">Fastasma</option>
+                            <option value="psiquico">Psíquico</option>
+                        </select>
+                    </div>
+
+                    <div class="form-floating m-2">
+                        <textarea class="form-control" placeholder="" name="descripcion-pokemon" required></textarea>
+                        <label for="floatingTextarea">Descripción del Pokémon</label>
+                    </div>
+
+                    <div class="form-floating m-2">
+                        <textarea class="form-control" placeholder="" style="height: 100px" name="informacion-pokemon" required></textarea>
+                        <label for="floatingTextarea2">Información del Pokémon</label>
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="formFile" class="form-label">Imagen del pokémon</label>
+                        <input class="form-control" type="file" id="formFile" required>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+        <div class="d-grid gap-2 col-3 mx-auto">
+            <button type="submit" class="btn btn-dark p-3">Agregar</button>
+        </div>
+
+    </form>
+
+
+<?php
+include ("../Layouts/footer.php");
+?>
